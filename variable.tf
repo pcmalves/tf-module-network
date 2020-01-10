8f9c1fc6d617f197e23185cf626f3e5bc4097076
Paulo Cezar Martins Alves <paulo.alves@redealumni.com>
2020-01-09 22:16:47 -0300

    added subnet in module

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
-variable "vpc_cidr_block" {}
-
-variable "enable_dns_support" {}
-
+variable "availability_zone" {}
 variable "enable_dns_hostnames" {}
-
+variable "enable_dns_support" {}
+variable "map_public_ip_on_launch" {}
+variable "subnet_cidr_block" {}
 variable "tag_name" {}
+variable "vpc_cidr_block" {}
